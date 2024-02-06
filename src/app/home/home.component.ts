2344ad497c0d5b18e31a52c845f14da4bcbd172d
Maximilian Niessl <contact@maxniessl.com>
2024-02-06 22:46:24 +0100

    Closed #9

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,28 @@
 // basic component
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+// router
+import { RouterLink } from "@angular/router";
 
 // services
-import { SharedDataService } from '../internal-services/shared-data.service';
+import { SharedDataService } from "../internal-services/shared-data.service";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: "app-home",
+    standalone: true,
+    imports: [CommonModule, RouterLink],
+    templateUrl: "./home.component.html",
+    styleUrl: "./home.component.css",
 })
 export class HomeComponent {
-  name: string;
-  
-  constructor(private sharedDataService: SharedDataService) {}
+    name: string;
 
-  ngOnInit() {
-    this.sharedDataService.getCookieName()
-      .subscribe(name => {
-        this.name = name;
-      });
-  }
+    constructor(private sharedDataService: SharedDataService) {}
+
+    ngOnInit() {
+        this.sharedDataService.getCookieName().subscribe((name) => {
+            this.name = name;
+        });
+    }
 }
