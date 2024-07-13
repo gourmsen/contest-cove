bfd92c140eb9a5bbb85a21a71df0325155ab5115
Maximilian Niessl <contact@maxniessl.com>
2024-07-13 20:27:19 +0200

    Added join button functionality

--- src/app/contest-join/contest-join.component.ts
+++ src/app/contest-join/contest-join.component.ts
@@ -3,14 +3,17 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 // router
-import { ActivatedRoute } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 // services
 import { SharedDataService } from "../internal-services/shared-data.service";
 import { ContestAttendeeListService } from "../http-services/contest-attendee-list.service";
+import { ContestJoinService } from "../http-services/contest-join.service";
 
 // interfaces
 import { ContestAttendeeListResponse } from "../interfaces/contest-attendee-list-response";
+import { ContestJoinRequest } from "../interfaces/contest-join-request";
+import { ContestJoinResponse } from "../interfaces/contest-join-response";
 
 @Component({
     selector: "app-contest-join",
@@ -21,27 +24,32 @@ import { ContestAttendeeListResponse } from "../interfaces/contest-attendee-list
 })
 export class ContestJoinComponent {
     contestAttendeeListResponseBody: ContestAttendeeListResponse;
+    contestJoinResponseBody: ContestJoinResponse;
 
     contestId: string;
     userId: string;
 
-    isAttendee: boolean;
+    loadingAttendees: boolean;
 
     constructor(
         private route: ActivatedRoute,
+        private router: Router,
         private sharedDataService: SharedDataService,
-        private contestAttendeeListService: ContestAttendeeListService
+        private contestAttendeeListService: ContestAttendeeListService,
+        private contestJoinService: ContestJoinService
     ) {}
 
     ngOnInit() {
         this.contestId = this.route.snapshot.paramMap.get("contestId") || "";
+
+        // loading
+        this.loadingAttendees = true;
 
         // get userId
         this.sharedDataService.getCookieUserId().subscribe((userId) => {
             this.userId = userId;
 
             // get attendeeList
-            this.isAttendee = false;
             this.contestAttendeeListService.listContestAttendees(this.contestId).subscribe(
                 (contestAttendeeListResponse) => {
                     this.contestAttendeeListResponseBody = contestAttendeeListResponse.body!;
@@ -49,16 +57,47 @@ export class ContestJoinComponent {
                     // check for attendee
                     this.contestAttendeeListResponseBody.data.attendees.forEach((attendee) => {
                         if (attendee.attendeeId === this.userId) {
-                            this.isAttendee = true;
+                            this.navigateToContest(this.contestId);
                         }
                     });
+
+                    this.loadingAttendees = false;
                 },
                 (error) => {
                     if (error.status === 404) {
-                        this.isAttendee = false;
                     }
+
+                    this.loadingAttendees = false;
                 }
             );
+        });
+    }
+
+    joinContest(contestId: string, attendeeId: string) {
+        // prepare request
+        let contestJoinRequest: ContestJoinRequest = {
+            contestId: contestId,
+            attendeeId: attendeeId,
+        };
+
+        // join contest
+        this.contestJoinService.joinContest(contestJoinRequest).subscribe(
+            (contestJoinResponse) => {
+                this.contestJoinResponseBody = contestJoinResponse.body!;
+            },
+            (error) => {
+                if (error.status === 409) {
+                }
+            }
+        );
+
+        this.navigateToContest(contestId);
+    }
+
+    navigateToContest(contestId: string) {
+        // navigate to contest
+        this.router.navigateByUrl("/", { skipLocationChange: true }).then(() => {
+            this.router.navigate(["contests/" + contestId]);
         });
     }
 }
